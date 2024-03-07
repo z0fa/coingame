8f47ec2a0f03d05ea5ad99072d6e43d04168ffff
Giovanni Cavallo <giovannicavallo95@gmail.com>
2024-03-07 13:04:53 +0100

    update artwork

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -2,17 +2,16 @@ function toss(val) {
   const coin = document.querySelector("#coin")
   const face = val < 0.5 ? "head" : "tail"
 
-  coin.classList.toggle("flipping", true)
-  coin.classList.toggle("head", false)
-  coin.classList.toggle("tail", false)
-  setTimeout(() => {
-    coin.classList.toggle(face, true)
-    coin.classList.toggle("flipping", false)
-  }, 1000)
+  if (coin.dataset.state === "flip") {
+    return
+  }
+
+  coin.dataset.state = "flip"
+  setTimeout(() => (coin.dataset.state = face), 1000)
 }
 
-const mc = new Hammer(document.querySelector("body"))
-
-mc.on("panup tap", function (event) {
-  toss(event.type === "panup" ? 1 : 0)
+const mc = new Hammer(window)
+mc.get("swipe").set({ direction: Hammer.DIRECTION_ALL })
+mc.on("swipe tap", function (event) {
+  toss(event.type === "swipe" ? 1 : 0)
 })
